87943a79f14a634d48f8ab3a2fc5b7b6df2ca2d1
Scott Werner <scott.werner.vt@gmail.com>
2018-09-20 12:44:45 -0400

    Require sass and not node-sass

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,12 +1,12 @@
 'use strict';
 
 const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
-const nodeSass = require('node-sass');
+const sass = require('sass');
 
 module.exports = function(defaults) {
   let app = new EmberAddon(defaults, {
     sassOptions: {
-      implementation: nodeSass,
+      implementation: sass,
     },
   });
 
